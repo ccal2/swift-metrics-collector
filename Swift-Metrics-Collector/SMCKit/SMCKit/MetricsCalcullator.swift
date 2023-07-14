5c1fc1f8d032e57359f9c1e93004116a7dc715e0
Carolina Lopes <23311541+ccal2@users.noreply.github.com>
2023-07-13 21:45:29 -0300

    Comment out original LCOM

--- Swift-Metrics-Collector/SMCKit/SMCKit/MetricsCalcullator.swift
+++ Swift-Metrics-Collector/SMCKit/SMCKit/MetricsCalcullator.swift
@@ -42,32 +42,32 @@ struct MetricsCalcullator {
         return calculateDIT(for: parent) + 1
     }
 
-    /// Consider a Class C with n methods M_1_,M_2_,...,M_n_. Let {I_i_} = set of instance variables used by method M_i_.
-    /// Let P = {(I_i_,I_j_) | I_i_ ∩ I_j_ = ∅} and Q = {(I_i_,I_j_) | I_i_ ∩ I_j_ != ∅. If all n sets {I_1_},...,{I_n_} are 0 then let P = ∅.
-    /// LCOM(C) =  |P| - |Q|, if |P| > |Q|
-    ///          0, otherwise
-    static func calculateLCOM(for typeNode: TypeNode) -> Int {
-        let methods = typeNode.instanceMethodsIncludingExtensions
-        let methodsCount = methods.count
-        var disjointSets = 0
-        var intersectingSets = 0
-
-        guard methodsCount > 0 else {
-            return 0
-        }
-
-        for i in 0 ..< methodsCount-1 {
-            for j in i+1 ..< methodsCount {
-                if methods[i].accessedInstanceVariables.isDisjoint(with: methods[j].accessedInstanceVariables) {
-                    disjointSets += 1
-                } else {
-                    intersectingSets += 1
-                }
-            }
-        }
-
-        return disjointSets > intersectingSets ? disjointSets - intersectingSets : 0
-    }
+//    /// Consider a Class C with n methods M_1_,M_2_,...,M_n_. Let {I_i_} = set of instance variables used by method M_i_.
+//    /// Let P = {(I_i_,I_j_) | I_i_ ∩ I_j_ = ∅} and Q = {(I_i_,I_j_) | I_i_ ∩ I_j_ != ∅. If all n sets {I_1_},...,{I_n_} are 0 then let P = ∅.
+//    /// LCOM(C) =  |P| - |Q|, if |P| > |Q|
+//    ///          0, otherwise
+//    static func calculateLCOM(for typeNode: TypeNode) -> Int {
+//        let methods = typeNode.instanceMethodsIncludingExtensions
+//        let methodsCount = methods.count
+//        var disjointSets = 0
+//        var intersectingSets = 0
+//
+//        guard methodsCount > 0 else {
+//            return 0
+//        }
+//
+//        for i in 0 ..< methodsCount-1 {
+//            for j in i+1 ..< methodsCount {
+//                if methods[i].accessedInstanceVariables.isDisjoint(with: methods[j].accessedInstanceVariables) {
+//                    disjointSets += 1
+//                } else {
+//                    intersectingSets += 1
+//                }
+//            }
+//        }
+//
+//        return disjointSets > intersectingSets ? disjointSets - intersectingSets : 0
+//    }
 
     /// Hitz and Montazeri LCOM
     /// Consider a Class X
